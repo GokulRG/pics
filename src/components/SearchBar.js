34cf2e6ca7f08c889c36fe5fb3504adad64a2788
Gokul Kramnik <ramakrishnan.go@husky.neu.edu>
2019-06-11 22:29:23 -0700

    Controlled SearchBar Component
    Converting the searchbar from an uncontrolled to a controlled component.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
+    state = { searchTerm: '' };
+
     onInputChange(event) {
-        console.log(event.target.value);
+        //Now the value in the input field is known only to the input HTML element and it's saved in the DOM
+        //We need to save that information in react, so that we don't have to work with the DOM every single 
+        //time and we can reap the benefits of React's state management
+        this.setState({searchTerm: event.target.value});
     }
 
     render() {
@@ -12,7 +17,8 @@ class SearchBar extends React.Component {
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search </label> <br/>
-                        <input type="text" onChange={this.onInputChange}/>
+                        {/* This is the syntax for passing a function with an argument */}
+                        <input type="text" value={this.state.searchTerm} onChange={event => this.onInputChange(event)}/>
                     </div>
                 </form>
             </div>
